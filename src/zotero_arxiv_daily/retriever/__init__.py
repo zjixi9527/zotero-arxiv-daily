881baef87ee9a6fc0192778f4fbb8de1ec37e718
jie zhong <wyyx_zjx@163.com>
2026-09-02 16:44:41 +0800

    Refactor imports in __init__.py for readability

--- src/zotero_arxiv_daily/retriever/__init__.py
+++ src/zotero_arxiv_daily/retriever/__init__.py
@@ -1,2 +1,9 @@
 from .base import get_retriever_cls
-from . import arxiv_retriever, biorxiv_retriever, medrxiv_retriever, chemrxiv_retriever
+
+from . import (
+    arxiv_retriever,
+    biorxiv_retriever,
+    medrxiv_retriever,
+    chemrxiv_retriever,
+    openalex_retriever,
+)
